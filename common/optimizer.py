da8b5a95423ca82add0c46f1a2ea653c3428888d
MADE IN HEAVEN <519334736@qq.com>
2020-08-25 16:35:05 +0800

    新增optimizer 并加入测试案例

---
+++ common/optimizer.py
@@ -0,0 +1,249 @@
+# coding: utf-8
+import numpy as np
+
+class SGD:
+
+    """随机梯度下降法（Stochastic Gradient Descent）"""
+
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.lr * grads[key] 
+
+
+class Momentum:
+
+    """Momentum SGD"""
+
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():                                
+                self.v[key] = np.zeros_like(val)
+                
+        for key in params.keys():
+            self.v[key] = self.momentum*self.v[key] - self.lr*grads[key] 
+            params[key] += self.v[key]
+
+
+class kMomentum:
+    '''k步-动量计算 k=1时同momentum k>1时每次动量经过k部计算更平缓 但更费时'''
+    def __init__(self, lr=0.01, momentum=0.9,k=1):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+        self.k = k
+        
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+            
+        for key in params.keys():
+            for _ in range(self.k):
+                self.v[key]  = self.momentum*self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
+            # params[key] += self.momentum * self.momentum * self.v[key]
+            # params[key] -= (1 + self.momentum) * self.lr * grads[key]
+
+
+
+class Nesterov:
+
+    """Nesterov's Accelerated Gradient (http://arxiv.org/abs/1212.0901)"""
+
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+            
+        for key in params.keys():
+            self.v[key] *= self.momentum
+            self.v[key] -= self.lr * grads[key]
+            params[key] += self.momentum * self.momentum * self.v[key]
+            params[key] -= (1 + self.momentum) * self.lr * grads[key]
+
+
+
+class kNesterov:
+
+    """k部nestrov 动量计算同momentum 梯度计算经过多次套娃"""
+
+    def __init__(self, lr=0.01, momentum=0.9,k=1):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+        self.k = k 
+        
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+            
+        for key in params.keys():
+            self.v[key] *= self.momentum
+            self.v[key] -= self.lr * grads[key]
+
+            temp = self.momentum*self.v[key] - self.lr*grads[key]
+            for _ in range(self.k-1):
+                temp = self.momentum*temp - self.lr*grads[key]
+            params[key] += temp
+            # params[key] += self.momentum * self.momentum * self.v[key]
+            # params[key] -= (1 + self.momentum) * self.lr * grads[key]
+
+
+
+
+
+
+
+class AdaGrad:
+
+    """AdaGrad"""
+
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        self.h = None
+        
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+            
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+
+
+class RMSprop:
+
+    """RMSprop"""
+
+    def __init__(self, lr=0.01, decay_rate = 0.99):
+        self.lr = lr
+        self.decay_rate = decay_rate
+        self.h = None
+        
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+            
+        for key in params.keys():
+            self.h[key] *= self.decay_rate
+            self.h[key] += (1 - self.decay_rate) * grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+
+
+class Adadelta:
+    def __init__(self,rho=0.95,eta=1e-5):
+        self.rho = rho 
+        self.eta = eta
+        self.Ex = None
+        self.Eg = None
+    
+    def update(self,params,grads):
+        if self.Ex is None:
+            self.Ex,self.Eg = {},{}
+            for k,v in params.items():
+                self.Ex[k] = np.zeros_like(v)
+                self.Eg[k] = np.zeros_like(v)
+        
+        for key in params.keys():
+            self.Eg[key] = self.rho*self.Eg[key] + (1-self.rho)*grads[key]**2
+            delta_x = -np.sqrt(self.Ex[key]+self.eta)* grads[key]/np.sqrt(self.Eg[key]+self.eta) 
+            self.Ex[key] = self.rho*self.Ex[key] + (1-self.rho)*delta_x**2
+            params[key] += delta_x 
+
+
+
+
+
+
+
+class Adam:
+
+    """Adam (http://arxiv.org/abs/1412.6980v8)"""
+
+    def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iter = 0
+        self.m = None
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.m is None:
+            self.m, self.v = {}, {}
+            for key, val in params.items():
+                self.m[key] = np.zeros_like(val)
+                self.v[key] = np.zeros_like(val)
+        
+        self.iter += 1
+        lr_t  = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)         
+        
+        for key in params.keys():
+            #self.m[key] = self.beta1*self.m[key] + (1-self.beta1)*grads[key]
+            #self.v[key] = self.beta2*self.v[key] + (1-self.beta2)*(grads[key]**2)
+            self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
+            self.v[key] += (1 - self.beta2) * (grads[key]**2 - self.v[key])
+            
+            params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
+            
+            #unbias_m += (1 - self.beta1) * (grads[key] - self.m[key]) # correct bias
+            #unbisa_b += (1 - self.beta2) * (grads[key]*grads[key] - self.v[key]) # correct bias
+            #params[key] += self.lr * unbias_m / (np.sqrt(unbisa_b) + 1e-7)
+
+
+class Adamax:
+
+    def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iter = 0
+        self.m = None
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.m is None:
+            self.m, self.v = {}, {}
+            for key, val in params.items():
+                self.m[key] = np.zeros_like(val)
+                self.v[key] = np.zeros_like(val)
+        
+        self.iter += 1
+        lr_t  = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)         
+        
+        for key in params.keys():
+            #self.m[key] = self.beta1*self.m[key] + (1-self.beta1)*grads[key]
+            #self.v[key] = self.beta2*self.v[key] + (1-self.beta2)*(grads[key]**2)
+            self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
+            self.v[key] = np.maximum(self.beta2*self.v[key],np.abs(grads[key]))
+            # self.v[key] += (1 - self.beta2) * (grads[key]**2 - self.v[key])
+
+
+            
+            params[key] -= lr_t * self.m[key] / self.v[key]
+            
+            #unbias_m += (1 - self.beta1) * (grads[key] - self.m[key]) # correct bias
+            #unbisa_b += (1 - self.beta2) * (grads[key]*grads[key] - self.v[key]) # correct bias
+            #params[key] += self.lr * unbias_m / (np.sqrt(unbisa_b) + 1e-7)
